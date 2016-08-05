8530a7f41e16beda54e63096de088900fa1f195a
sakshiganeriwal <sakshi.ganeriwal2012@vit.ac.in>
2016-08-05 14:05:21 +0530

    update22

--- Rack.java
+++ Rack.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.util.Arrays;
 
 public class Rack {
 	Tiles[] rack = new Tiles[7];
@@ -11,6 +13,91 @@ public class Rack {
 		maxWord = "";
 		maxScore = 0;
 	}
+	
+	public static void Combination()
+	{
+		private static void comb2(String prefix, String s) {
+	        System.out.println(prefix);
+	        for (int i = 0; i < s.length(); i++)
+	            comb2(prefix + s.charAt(i), s.substring(i + 1));
+	}
+	
+	public static String Permutation(Tile<> tile,int k,Tile<> perm)
+	{
+	        if (k == tile.length) 
+	        {
+	            for (int i = 0; i < tile.length; i++) 
+	            {
+	                perm[i].letter=tile[i].letter;
+	                return Perm;
+	            }
+	        } 
+	        else 
+	        {
+	            for (int i = k; i < tile.length; i++) 
+	            {
+	                char temp = tile[k].letter;
+	                tile[k].letter = a[i].letter;
+	                tile[i].letter = temp;
+	 
+	                Permutation(tile, k + 1,perm);
+	 
+	                temp = tile[k].letter;
+	                tile[k].letter = tile[i].letter;
+	                tile[i].letter = temp;
+	            }
+	        }
+	}
+	
+	
+	public static String sortLetters(String word){
+		String sortedWord="";
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        sortedWord = new String(chars);
+        return sortedWord;
+	}
+	
+	public static void permutation(String str) { 
+		String sortWord = sortLetters(str);
+		genanagrams(str); 
+	}
+
+	private static void genanagrams(String str) {
+	    int n = str.length();
+	    if (n == 0) System.out.println(prefix);
+	    else {
+	        for (int i = 0; i < str.length; i++)
+	            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+	    }
+	}
+		
+	public static boolean isValidWord(Tiles[] tile ){
+			
+			String word="";
+			
+			for(int i = 0;i < tile.length; i++ ){
+				
+				word += (tile[i].letter); 
+				
+			}
+			System.out.println(word);
+			try{
+				Scanner s = new Scanner(new File("C:/Users/sganeriwal/Desktop/PYPLtraining/Anagrams/sowpods.txt"));
+				String line ="";
+				while(s.hasNextLine()){
+					line = s.nextLine();
+					if(line.equalsIgnoreCase(word)){
+						return true;
+					}
+				}
+			}
+			catch(Exception E){
+				System.out.println("Exception"+E);
+			}
+			
+			return false;
+		}
 	
 	public static int calcScore(Tiles[] tile){
 		int score = 0;
@@ -33,7 +120,7 @@ public class Rack {
 	public static void main(String[] args){
 		String input = args[0];
 		Rack r = new Rack(input);
-		
+		r.combination();
 	}
 
 }
